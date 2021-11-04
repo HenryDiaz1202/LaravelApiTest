f1f52d8a94a6278b28a67fb19823a0ff3ca8229f
HenryDiaz1202 <henrydiaz1202@gmail.com>
2021-11-03 20:32:41 -0600

    modelo creado

--- app/Models/Tareas.php
+++ app/Models/Tareas.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Tareas extends Model
 {
     use HasFactory;
+
+    protected $table = 'tareas';
+    protected $fillable = [
+        'nombre',
+        'descripcion'
+    ];
 }
